6499d53e1eb4ea016ec87eecc4b64560b5821380
MasterDenoe <Dean1330@gmail.com>
2024-07-04 18:39:55 +1000

    conceptionPhase

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+print("*********************")
+print("Hello, Master Denoe!!")
+print("*********************")
+print("")
+
+count = 0
+for x in range(0, 1000):
+    if (x % 100) == 0:
+        count += 1
+        print(count)
+    
+
+print("")
+print("*********************")
+print("Program Finished!!!!!")
+print("*********************")
