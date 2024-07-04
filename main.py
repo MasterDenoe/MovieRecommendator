a0c991f60c84430449b9e0970f2ebe6e16bcdb9a
MasterDenoe <Dean1330@gmail.com>
2024-07-04 18:59:33 +1000

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-
+"""
+Started on 04/07/2024 by Dean Price
+"""
 
 print("*********************")
 print("Hello, Master Denoe!!")
 print("*********************")
 print("")
+
+# mission critical data
+viewerRating = 0
+criticRating = 0
+
+avgViewerRating = 0
+avgCriticRating = 0
+yearMade = 2000
+genre = {"adventure", "comedy", "drama", "action", "thriller", "documentry", "crime", "family"}
+budget = 1000000
+
+# misc data (not critical but may be a factor)
+runtime = 120
+
+
+
 
 count = 0
 for x in range(0, 1000):
